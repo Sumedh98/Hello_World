a1a4fba354f6679757e10ad1ab694db9ff1e23d8
Patanjali Joshi <86454491+Patanjalijoshi@users.noreply.github.com>
2022-07-26 12:09:59 +0530

    Added some random functionality

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
-print( 'Hello world')
+print('Hello world')
 print("Hello")
+
+prints = input("Enter a number")
+
+
+def loop(prints):
+    for i in range(prints):
+        print('hello')
+
+
+loop(prints)
